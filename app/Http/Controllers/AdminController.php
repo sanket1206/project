1619e73827d91251c939c307736a7604361e0a72
sanket1206 <smaisuriya1206@gmail.com>
2023-04-06 13:50:50 +0530

    first commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Controllers\AdminController;
+
+use Illuminate\Http\Request;
+use App\Models\Doctor;
+use App\Models\Appointment;
+class AdminController extends Controller
+{
+    public function addview(){
+
+        return view('admin.add_doctor');
+    }
+
+    public function upload(Request $request){
+
+        $doctor=new doctor;
+
+        $image=$request->file;
+        $imagename=time().'.'.$image->getClientoriginalExtension();
+        $request->file->move('doctorimage',$imagename);
+        $doctor->image=$imagename;
+
+        $doctor->name=$request->name;
+        $doctor->phone=$request->number;
+        $doctor->room=$request->room;
+        $doctor->speciality=$request->speciality;
+
+        $doctor->save();
+        return redirect()->back()->with('message','Doctor Added Successfully');
+        
+
+    }
+
+    public function showappointment(){
+
+
+        $data=appointment::all();
+
+        return view('admin.showappointment',compact('data'));
+    }
+    public function approved($id){
+
+        $data=appointment::find($id);
+
+        $data->status='approved';
+        $data->save();
+        return redirect()->back();
+        
+    }
+    public function cancle($id){
+
+        $data=appointment::find($id);
+
+        $data->status='cancle';
+        $data->save();
+        return redirect()->back();
+        
+    }
+
+    public function showdoctor(){
+
+
+        $data=doctor::all();
+
+        return view('admin.showdoctor',compact('data'));
+    }
+    public function deletedoctor($id){
+
+
+        $data=doctor::find($id);
+
+        $data->delete();
+
+        return redirect()->back();
+    }
+
+    public function updatedoctor($id){
+
+
+        $data=doctor::find($id);
+
+        return view('admin.update_doctor',compact('data'));
+
+
+    }
+
+    public function editdoctor(Request $request,$id){
+
+
+        $doctor =doctor::find($id);
+
+        $doctor->name=$request->name;
+        $doctor->phone=$request->number;
+        $doctor->speciality=$request->speciality;
+        $doctor->room=$request->room;
+
+        $image=$request->file;
+        if($image){
+        $imagename=time().'.'.$image->getClientOriginalExtension();
+        $request->file->move('doctorimage',$imagename);
+        $doctor->image=$imagename;
+        }
+        $doctor->save();
+
+        return redirect()->back()->with('message','Doctor Detail Updated Successfully');
+
+
+     }
+}
